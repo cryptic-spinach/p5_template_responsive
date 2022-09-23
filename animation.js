86b489967a5a166334a96e4292b30f24ac819bd9
Chase Reynolds <chase@Chases-MBP.home>
2022-09-22 20:40:30 -0400

    Dynamically select width or height to drive canvas size

--- animation.js
+++ animation.js
@@ -1,18 +1,34 @@
 let graphics;
 
 function setup() {
-  createCanvas(windowHeight*1.77, windowHeight);
+  if (windowWidth/windowHeight > 1.77) {
+    createCanvas(windowHeight*1.77, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth/1.77);
+  }
 
   graphics = createGraphics(width, height);
-  graphics.background(100);
 }
 
 function draw() {
-  graphics.ellipse(300, 300, 150, 150);
+  background("#131626");
+
+  graphics.clear();
+
+  graphics.noStroke();
+  graphics.fill("#0ED0A9")
+  graphics.ellipse(400, 200, 150, 150);
 
   image(graphics, 0, 0, width, height);
+  graphics.reset();
 }
 
 function windowResized() {
-  resizeCanvas(windowHeight*1.77, windowHeight);
+  if (windowWidth/windowHeight > 1.77) {
+    resizeCanvas(windowHeight*1.77, windowHeight);
+  }
+  else {
+    resizeCanvas(windowWidth, windowWidth/1.77);
+  }
 }
